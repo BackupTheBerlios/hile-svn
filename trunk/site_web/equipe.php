7cd6298bc36d0025684df490bdd7ce97b276a4a6
toutoune <toutoune@e89db83e-3701-0410-b7a8-e3ac3e734695>
2005-11-10 00:00:28 +0000

    Ajout de la presentation de l'equipe Mise a jour du tableau des rendus Evolution de la feuille de style Ajout d'entrees dans le menu

---
+++ trunk/site_web/equipe.php
@@ -0,0 +1,30 @@
+<?php
+  include('top.html');
+?>
+
+<h3>L'equipe</h3>
+   <p>Notre équipe est composée de cinq membres entre lesquels ont été réparties différentes responsabilités afin de mener à bien ce projet. Nous nous sommes basés sur l'expérience de chacun des membres de l'équipe pour les partager au mieux.</p>
+   <p>Les rôles ont donc été répartis en fonction des qualités de chacun :</p>
+
+   <p>Chef de projet : son travail consiste à diriger l'ensemble de l'équipe sur le projet. Il doit élaborer un planning afin de coordonner les différentes tâches et les répartir entre les membres de l'équipe. Il doit également savoir prendre les décisions nécessaires au bon déroulement du projet afin de respecter les délais impartis.</br>
+   Personne en charge : Romain Papuchon</p>
+
+   <p>Responsable client : il est l'intermédiaire entre l'équipe projet et les différents clients du projet Hile. Il doit permettre à son équipe de comprendre les différents besoins du client puisqu'il sera le principal intervenant auprès de ceux-ci.</br>
+Responsable du site Web : son rôle est de mettre en place une plateforme Web qui constituera dans un premier temps une vitrine pour le projet Hile mais également un moyen de communication entre les membres de l'équipe. Le site permettra entre autre de stocker tous les documents et procédures relatifs au projet Hile.</br>
+	Personne en charge : Antoine Mottier</p>
+
+   <p>Responsable technique : il doit rechercher les différentes solutions techniques qui permettront de répondre aux besoins du client. Ce rôle implique d'être constamment en avance sur les différents développements.</br>
+   Responsable développement : il a en charge de coordonner les différents développements et d'instaurer un certain nombre de règles de qualité afin de régir les nombreuses phases de code. Il doit également s'assurer que l'état d'avancement du développement respecte bien les spécifications établies pendant la phase de lancement du projet.</br>
+	Personne en charge : Olivier Boitel</p>
+	
+   <p>Responsable interface graphique : il est responsable de l'ergonomie générale du logiciel. Il doit s'assurer que l'interface graphique permet au client d'exploiter au mieux les fonctionnalités du logiciel de manières simple et intuitives.</br>
+   Secrétaire : il est en charge de rédiger un compte rendu de chaque réunion effectuée lors du projet afin de garder un suivi sur ses différentes étapes. Pour cela, il devra savoir sélectionner les informations essentielles et les organiser afin de retranscrire les points abordés.</br>
+	Personne en charge : Gérald Masquelier</p>
+	
+   <p>Responsable qualité : Il a pour fonction de gérer toute la documentation relative au projet. En effet, il doit fournir des modèles pour la rédaction des différents documents que l'équipe fournira et s'assurer de la lisibilité et de la clarté de ceux-ci.</br>
+   Responsable des tests : il doit fournir des procédures de tests qui permettront de valider les différents développements par rapport aux spécifications de départ. Il devra donc établir des rapports précis qui permettront à l'équipe de s'orienter vers les objectifs du projet.</br>
+	Personne en charge : Denis Guillon</p>
+
+<?php
+  include('bottom.html');
+?>
--- trunk/site_web/rendus.php
+++ trunk/site_web/rendus.php
@@ -3,14 +3,219 @@
 ?>
 
 <h1>Rendus</h1>
-<b>Le 20/10/2005 :</b><p> <a href="rendus/phase2/20051020/">cahier des charges fonctionnel</a>, <a href="rendus/phase2/20051020/HILE_pre_CDCT_V1.pdf">pré cahier des charges technique</a>, une description des <a href="rendus/phase2/20051020/HILE_outils_de_gestion_de_projet_V1.pdf">outils utilisés</a> et <a href="rendus/phase2/20051020/HILE_etat_avancement_P2_V1.pdf">l'état d'avancement</a>.</p>
-<b>Le 13/10/2005 :</b><p> nouvelle version du <a href="rendus/phase1/20051010/HILE_cahier_des_charges_V2.pdf">cahier des charges</a> et de <a href="rendus/phase1/20051010/HILE_etat_avancement_P1_V2.pdf">l'état d'avancement</a>.</p>
-<b>Le 06/10/2005 :</b><p> <a href="rendus/phase1/20051006/HILE_cahier_des_charges_V1.pdf">cahier des charges</a> et de <a href="rendus/phase1/20051006/HILE_etat_avancement_P1_V1.pdf">l'état d'avancement</a>.</p>
+Tous les documents réalisés peuvent être consultés. Ils sont classés par phases puis par date :
+<ul>
+	<li><a href="#phase3">Phase 3</a></li>
+	<li><a href="#phase2">Phase 2</a></li>
+	<li><a href="#phase1">Phase 1</a></li>
+</ul>
+Les <a href="#reunions_clients">comptes-rendus</a> des rénuions clients sont également disponible.
 
+<table class="report">
+	<thead>
+		<tr>
+			<th colspan="3" class="report"><a name="phase3" />Phase 3</th>
+		</tr>
+	</thead>
+	<tbody>
+		<tr>
+			<td class="report">10/11/2005</td>
+			<td class="report"><a href="rendus/phase3/20051110/">Cahier des charges graphique</a></td>
+			<td class="report">Pr&eacute;sente la charte graphique ainsi que le d&eacute;coupage de Hile en diff&eacute;rentes
+			zones pour un client l&eacute;ger et un client riche</td>
+		</tr>
+		<tr>
+			<td class="report">10/11/2005</td>
+			<td class="report"><a href="rendus/phase3/20051110/">Etat d'avancement 3</a></td>
+			<td class="report">Utilisation des ressources depuis le rendu du cahier des charges fonctionnel jusqu'au rendu du
+			cahier des charges graphique. Planification de la phase 4, c'est &agrave; dire depuis le rendu du cahier des charges
+			graphique jusqu'au rendu du cahier des charges technique</td>
+		</tr>
+		<tr>
+			<td class="report">08/11/2005</td>
+			<td class="report"><a href="rendus/phase3/20051108/sebastien_lamps.pdf">Int&eacute;gration de S&eacute;bastien Lamps</a></td>
+			<td class="report">Compte rendu de l'int&eacute;gration de S&eacute;bastien Lamps dans l'&eacute;quipe du projet Hile</td>
+		</tr>
+		<tr>
+			<td class="report">03/11/2005</td>
+			<td class="report"><a href="rendus/phase2/20051103/HILE_cahier_des_charges_fonctionnel_V2.pdf">Cahier des charges fonctionnel V2</a></td>
+			<td class="report">Reprise de la version 1 suite aux remarques des charg&eacute;s de travaux dirig&eacute;s</td>
+		</tr>
+		<tr>
+			<td class="report">02/11/2005</td>
+			<td class="report"><a href="compte_rendu/internes/20051102/cr9.pdf">CR interne n&deg;9</a></td>
+			<td class="report">Point sur l'&eacute;tat d'avancement de l'ensemble des documentations</td>
+		</tr>
+		<tr>
+			<td class="report">27/10/2005</td>
+			<td class="report"><a href="compte_rendu/internes/20051027/cr8.pdf">CR interne n&deg;8</a></td>
+			<td class="report">Point sur l'ensemble des d&eacute;sirs et remarques pour le design et choix du style graphique
+			pour l'application</td>
+		</tr>
+		<tr>
+			<td class="report">25/10/2005</td>
+			<td class="report"><a href="rendus/phase3/20051025/HILE_entretien_individuel_1.pdf">Entretien Individuel n&deg;1</a></td>
+			<td class="report">R&eacute;capitulatif et bilan des entretiens individuels fait avec l'&eacute;quipe Hawa&iuml; Team
+			le 25 octobre 2005</td>
+		</tr>
+	</tbody>
+</table>
 
+<table class="report">
+	<thead>
+		<tr>
+			<th colspan="3" class="report"><a name="phase2" />Phase 2</th>
+		</tr>
+	</thead>
+	<tbody>
+		<tr>
+			<td class="report">20/11/2005</td>
+			<td class="report"><a href="rendus/phase2/20051020/HILE_cahier_des_charges_fonctionnel_V1.pdf">Cahier des charges fonctionnel</a></td>
+			<td class="report">Exprime et Analyse les besoins des utilisateurs de fa&ccedil;on pr&eacute;cise en utilisant des
+			outils qui sont le d&eacute;coupage en paquetages et en cas d&rsquo;utilisation</td>
+		</tr>
+		<tr>
+			<td class="report">20/10/2005</td>
+			<td class="report"><a href="rendus/phase2/20051020/HILE_etat_avancement_P2_V1.pdf">Etat d'avancement 2</a></td>
+			<td class="report">Utilisation des ressources depuis le rendu du cahier des charges jusqu'au rendu du cahier des
+			charges fonctionnel. Planification de la phase 3, c'est &agrave; dire depuis le rendu du cahier des charges
+			fonctionnel jusqu'au rendu du cahier des charges technique</td>
+		</tr>
+		<tr>
+			<td class="report">19/10/2005</td>
+			<td class="report"><a href="rendus/phase2/20051020/HILE_pre_CDCT_V1.pdf">Pr&eacute;-cahier des charges technique</a></td>
+			<td class="report">Ce document est une &eacute;tude pr&eacute;liminaire des diff&eacute;rents moyens techniques
+			disponibles pour r&eacute;aliser le projet Hile. Il pr&eacute;c&egrave;de le cahier des charges technique dans lequel
+			des choix seront faits pour la r&eacute;alisation et la mise en place du projet.</td>
+		</tr>
+		<tr>
+			<td class="report">19/10/2005</td>
+			<td class="report"><a href="rendus/phase2/20051020/HILE_outils_de_gestion_de_projet_V1.pdf">Outils de gestion de projet</a></td>
+			<td class="report">Ce document pr&eacute;sente les diff&eacute;rents outils de gestion et de suivi de projets
+			utilis&eacute;s pour la r&eacute;alisation du projet Hile.</td>
+		</tr>
+		<tr>
+			<td class="report">12/10/2005</td>
+			<td class="report"><a href="rendus/phase1/20051010/HILE_cahier_des_charges_V2.pdf">Cahier des charges V2</a></td>
+			<td class="report">Reprise de la version 1 suite aux remarques des charg&eacute;s de travaux dirig&eacute;s</td>
+		</tr>
+		<tr>
+			<td class="report">10/10/2005</td>
+			<td class="report"><a href="rendus/phase1/20051010/HILE_etat_avancement_P1_V2.pdf">Etat d'avancement 1 - V2</a></td>
+			<td class="report">Reprise de la version 1 suite aux remarques des charg&eacute;s de travaux dirig&eacute;s</td>
+		</tr>
+		<tr>
+			<td class="report">10/10/2005</td>
+			<td class="report"><a href="compte_rendu/internes/20051010/cr7.pdf">CR interne n&deg;7</a></td>
+			<td class="report">Regroupement des points n&eacute;gatifs du cahier des charges et l'&eacute;laboration d'un nouveau
+			plan, suite aux diff&eacute;rentes remarques faites par les charg&eacute;s de Travaux Dirig&eacute;s</td>
+		</tr>
+	</tbody>
+</table>
 
+<table class="report">
+	<thead>
+		<tr>
+			<th colspan="3" class="report"><a name="phase1" />Phase 1</th>
+		</tr>
+	</thead>
+	<tbody>
+		<tr>
+			<td class="report">06/10/2005</td>
+			<td class="report"><a href="rendus/phase1/20051006/HILE_cahier_des_charges_V1.pdf">Cahier des charges</a></td>
+			<td class="report">Bilan exhaustif de l'expression de besoins client dans le cadre de la refonte de l'application
+			Gala. S'appuie sur la des<a href="">CRiption de l'ensemble des objectifs du projet Hile et de ses contributeurs</td>
+		</tr>
+		<tr>
+			<td class="report">05/10/2005</td>
+			<td class="report"><a href="rendus/phase1/20051006/HILE_etat_avancement_P1_V1.pdf">Etat d'avancement 1</a></td>
+			<td class="report">Utilisation des ressources depuis le d&eacute;but du projet jusqu'au rendu du cahier des charges
+			Planification de la phase 2, c'est &agrave; dire depuis le rendu du cahier des charges jusqu'au rendu du cahier des
+			charges fonctionnel</td>
+		</tr>
+		<tr>
+			<td class="report">04/10/2005</td>
+			<td class="report"><a href="compte_rendu/internes/20051004/cr6.pdf">CR interne n&deg;6</a></td>
+			<td class="report">Point sur les diff&eacute;rentes parties du cahier des charges d&eacute;j&agrave;
+			r&eacute;dig&eacute;es</td>
+		</tr>
+		<tr>
+			<td class="report">03/10/2005</td>
+			<td class="report"><a href="compte_rendu/internes/20051003/cr5.pdf">CR interne n&deg;5</a></td>
+			<td class="report">Gestion des r&eacute;pertoires dans SVN R&eacute;organisation des r&eacute;pertoires Remarques sur
+			l'esquisse du logo afin de le faire &eacute;voluer et de le terminer Finaliser les templates</td>
+		</tr>
+		<tr>
+			<td class="report">28/09/2005</td>
+			<td class="report"><a href="compte_rendu/internes/20050928/cr4.pdf">CR interne n&deg;4</a></td>
+			<td class="report">Choix des solutions pour le cahier des charges Choix si cr&eacute;ation ou non de la base
+			Recrutement en plus de la base Gala D&eacute;but de la liste des acteurs D&eacute;but du diagramme d'interactions
+			D&eacute;but du glossaire</td>
+		</tr>
+		<tr>
+			<td class="report">27/09/2005</td>
+			<td class="report"><a href="compte_rendu/internes/20050927/cr3.pdf">CR interne n&deg;3</a></td>
+			<td class="report">D&eacute;marrage du cahier des charges Planification des nouvelles t&acirc;ches attribu&eacute;es
+			&agrave; chaque membre de l'&eacute;quipe</td>
+		</tr>
+		<tr>
+			<td class="report">26/09/2005</td>
+			<td class="report"><a href="compte_rendu/internes/20050926/cr2.pdf">CR interne n&deg;2</a></td>
+			<td class="report">Choix du Wiki &agrave; utiliser Point sur la premi&egrave;re r&eacute;union avec les clients
+			Planification des nouvelles t&acirc;ches pour chacun des membres de l'&eacute;quipe</td>
+		</tr>
+		<tr>
+			<td class="report">20/09/2005</td>
+			<td class="report"><a href="compte_rendu/internes/20050920/cr1.pdf">CR interne n&deg;1</a></td>
+			<td class="report">R&eacute;union de d&eacute;part pour fixer les objectifs</td>
+		</tr>
+	</tbody>
+</table>
 
-<p>Voir les <a href="./compte_rendu">comptes rendus</a> des réunions avec les clients et des réunions internes.</p>
+<table class="report">
+	<thead>
+		<tr>
+			<th colspan="3" class="report"><a name="reunions_clients"/>Comptes-rendus  réunions clients</th>
+		</tr>
+	</thead>
+	<tbody>
+		<tr>
+			<td class="report">30/09/2005</td>
+			<td class="report"><a href="compte_rendu/clients/administrateur/cr20050930.pdf">administrateur</a></td>
+			<td class="report">R&eacute;union avec l'administrateur</td>
+		</tr>
+		<tr>
+			<td class="report">29/09/2005</td>
+			<td class="report"><a href="compte_rendu/clients/cellule_alternance/cr20050929.pdf">CR cellule alternance</a></td>
+			<td class="report">R&eacute;union avec la cellule alternance</td>
+		</tr>
+		<tr>
+			<td class="report">03/10/2005</td>
+			<td class="report"><a href="compte_rendu/clients/pole_apprenti/cr20051003.pdf">CR p&ocirc;le apprenti</a></td>
+			<td class="report">R&eacute;union avec le p&ocirc;le apprenti</td>
+		</tr>
+		<tr>
+			<td class="report">28/09/2005</td>
+			<td class="report"><a href="compte_rendu/clients/pole_ecole/cr20050928.pdf">CR p&ocirc;le &eacute;cole</a></td>
+			<td class="report">R&eacute;union avec le p&ocirc;le &eacute;cole</td>
+		</tr>
+		<tr>
+			<td class="report">22/09/2005</td>
+			<td class="report"><a href="compte_rendu/clients/poles_recrutement_entreprise/cr20050922_clients2.pdf">CR p&ocirc;le recrutement / entreprise</a></td>
+			<td class="report">R&eacute;unions avec les p&ocirc;les recrutement et entreprise</td>
+		</tr>
+		<tr>
+			<td class="report">09/2005</td>
+			<td class="report"><a href="compte_rendu/clients/responsable_filiere/cr200509.pdf">CR responsable fili&egrave;re</a></td>
+			<td class="report">R&eacute;union avec le responsable fili&egrave;re</td>
+		</tr>
+		<tr>
+			<td class="report">22/09/2005</td>
+			<td class="report"><a href="compte_rendu/clients/poles_recrutement_entreprise/cr20050922_clients1.pdf">CR secr&eacute;taires &eacute;cole</a></td>
+			<td class="report">R&eacute;union avec le responsable fili&egrave;re</td>
+		</tr>
+	</tbody>
+</table>
 
 <?php
   include('bottom.html');
